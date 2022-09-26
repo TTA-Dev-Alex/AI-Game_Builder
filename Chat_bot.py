b351f4e84e094f1379f0be9f4379f432257630c4
TTA-Dev-Alex <114209303+TTA-Dev-Alex@users.noreply.github.com>
2022-09-26 16:47:52 +0700

    Create Chat_bot.py

---
+++ Chat_bot.py
@@ -0,0 +1,10 @@
+name = input("Hello! Whats your name?")
+print("Nice to meet you " + (name))
+
+home = input("Where are you from " + (name) + "?")
+print("Wow! I'd love to visit " + (home))
+
+food = input("So " + name + ", from " + home + ", whats your favorite food?")
+print(food + " sounds good! Too bad robots cant taste :(")
+
+print("It's been great getting to know you " + name + ". I have to go to sleep now. Goodbye!")
